d002476e5954d5b47728d3cf9bc646d731ce1da5
Ali Shabani <alishabani9270@gmail.com>
2024-05-06 09:43:12 +0330

    wip

--- .github/scripts/substitute-env-vars.sh
+++ .github/scripts/substitute-env-vars.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-# Check if a directory has been provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <directory>"
-  exit 1
-fi
+# Initialize variables
+DIRECTORY=""
+declare -a EXCLUDE_PATTERNS
 
-DIRECTORY=$1
+# Usage message
+usage() {
+  echo "Usage: $0 -d <directory> [-e <exclude-patterns>]"
+  echo "Exclude patterns should be separated by commas."
+  exit 1
+}
+
+# Parse command line options
+while getopts "d:e:" opt; do
+  case $opt in
+    d) DIRECTORY=$OPTARG ;;
+    e) IFS=',' read -r -a EXCLUDE_PATTERNS <<< "$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+# Check if a directory has been provided
+if [ -z "$DIRECTORY" ]; then
+  usage
+fi
 
 # Check if the directory exists
 if [ ! -d "$DIRECTORY" ]; then
@@ -14,14 +31,26 @@ if [ ! -d "$DIRECTORY" ]; then
   exit 1
 fi
 
-# Iterate over all files in the directory
-for file in "$DIRECTORY"/*; do
-  # Check if the current item is a file
-  if [ -f "$file" ]; then
-    echo "Processing $file"
-    # Substitute environment variables and write to a temporary file
-    envsubst < "$file" > "$file.tmp"
-    # Move the temporary file to replace the original file
-    mv "$file.tmp" "$file"
+# Function to check if a file matches any exclude patterns
+matches_exclude_pattern() {
+  local filename=$1
+  for pattern in "${EXCLUDE_PATTERNS[@]}"; do
+    if [[ $filename == *$pattern* ]]; then
+      return 0 # True, pattern matches
+    fi
+  done
+  return 1 # False, no pattern matches
+}
+
+# Iterate over all files in the directory and its subdirectories
+find "$DIRECTORY" -type f | while read -r file; do
+  if matches_exclude_pattern "$file"; then
+    echo "Excluding $file"
+    continue
   fi
+  echo "Processing $file"
+  # Substitute environment variables and write to a temporary file
+  envsubst < "$file" > "$file.tmp"
+  # Move the temporary file to replace the original file
+  mv "$file.tmp" "$file"
 done
