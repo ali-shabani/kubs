904a0fb9c8736fc01f2a32b375d6ec403b8b0127
ali shabani <alishabani9270@gmail.com>
2024-05-17 15:22:36 +0330

    wip

--- deployment/docker/services/db-backup/entrypoint.sh
+++ deployment/docker/services/db-backup/entrypoint.sh
@@ -7,4 +7,4 @@ PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h postgres -U $POSTGRES_USER -d buynow -F
 timestamp=$(date +%Y%m%d%H%M%S)
 
 # Upload the dump to S3
-aws s3 cp /tmp/backup.tar s3://$S3_BUCKET_NAME/backup_$timestamp.tar --endpoint-url $AWS_ENDPOINT_URL
+aws s3 cp /tmp/backup.tar s3://$S3_BUCKET/backup_$timestamp.tar --endpoint-url $AWS_ENDPOINT_URL
