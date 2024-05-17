c1965ce2abc82679205c9cf3c3ab9baab4e60552
ali shabani <alishabani9270@gmail.com>
2024-05-17 14:56:01 +0330

    ignore crontab

--- deployment/docker/services/db-backup/entrypoint.sh
+++ deployment/docker/services/db-backup/entrypoint.sh
@@ -3,5 +3,8 @@
 # Use pg_dump to dump the database
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h postgres -U $POSTGRES_USER -d buynow -F t > /tmp/backup.tar
 
+# Get the current timestamp
+timestamp=$(date +%Y%m%d%H%M%S)
+
 # Upload the dump to S3
-aws s3 cp /tmp/backup.tar s3://$S3_BUCKET_NAME/backup.tar --endpoint-url $AWS_ENDPOINT_URL
+aws s3 cp /tmp/backup.tar s3://$S3_BUCKET_NAME/backup_$timestamp.tar --endpoint-url $AWS_ENDPOINT_URL
